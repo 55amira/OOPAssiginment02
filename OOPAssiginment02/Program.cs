979020cb353eaefdba51a8612bd25f0d15da54f1
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-04 13:41:58 +0200

    inhertence

--- OOPAssiginment02/Program.cs
+++ OOPAssiginment02/Program.cs
@@ -29,7 +29,43 @@
         //}
         static void Main(string[] args)
         {
-            
+            //inheritance
+
+            // Parent parent = new Parent(1, 2);
+            // Console.WriteLine(parent);
+
+            // parent.Fun01();
+            //parent.Fun02();
+
+            // Child child=new Child(1,2,3);
+            //// Console.WriteLine(child);
+            // child.Fun01();
+            // child.Fun02();
+
+            //Sum(1, 2);
+            //Sum(1.3, 4.3);
+
+            // TypeB typeB = new TypeB() { A = 12, B = 13 };
+            //typeB.A = 12;
+            //typeB.B = 13;
+
+            // Console.WriteLine("Press any key to exit...");
+            // Console.ReadKey();
+
+            //typeB.Fun01();
+            //typeB.Fun02();
+
+            //TypeA Ref;
+            //Ref = new TypeA();
+            //Ref = new TypeB();
+
+            // Ref.A = 12;
+            //  Ref.B = 13;
+            // 
+            // Ref.Fun01();
+            // Ref.Fun02();
+            //
+
         }
     }
 }
---
+++ OOPAssiginment02/inheritance/TypeB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAssiginment02.inheritance
+{
+    internal class TypeB : TypeA
+    {
+        public int B { get; set; }
+
+        public new void Fun01()
+        {
+            Console.WriteLine("Fun01 From TypeB ");
+        }
+
+        public override void Fun02()
+        {
+            Console.WriteLine($"A :: {A} B :: {B} ");
+        }
+
+
+
+    }
+}
