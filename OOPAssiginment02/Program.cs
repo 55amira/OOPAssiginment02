9be3161cfb8c991956c4c447b43dc0ff7f3fde1b
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-04 13:37:22 +0200

    Polymorphsim

--- OOPAssiginment02/Program.cs
+++ OOPAssiginment02/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+
+        //static int Sum(int X, int Y)
+        //{
+        //    return X + Y;
+        //}
+
+        //static int Sum(int X, int Y, int Z)
+        //{
+        //    return X + Y + Z;
+        //}
+
+        //static double Sum(double X, double Y)
+        //{
+        //    return X + Y;
+        //}
+
+        //static double Sum(int X, double Y)
+        //{
+        //    return X + Y;
+        //}
+
+        //static double Sum(double X, int Y)
+        //{
+        //    return X + Y;
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
